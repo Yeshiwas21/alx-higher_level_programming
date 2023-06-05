3e58aea533e54b974672f2a8c13663716080da4d
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-06-05 15:41:47 +0300

    Copy - Cut - Paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"First 3 letters: {word[0:3]}")
-print(f"Last 2 letters: {word[7:]}")
-print(f"Middle word: {word[1:8]}")
+word_first_3 = f"{word[0:3]}"
+word_last_2 = f"{word[-2:]}"
+middle_word = f"{word[1:-1]}"
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
