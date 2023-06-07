f8ff23dfae47a4f46260f5a9da757008601627ab
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-06-07 05:47:01 +0300

    ToUpperCase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for chr in str:
         if ord(chr) > 96 and ord(chr) < 123:
-            c = chr(ord(chr) - 32)
+            chr = chr(ord(chr) - 32)
         print("{}".format(chr), end="")
     print("")
 
