ebb7df3f31c875fe8623b6fd9853c0bb2d6d26dd
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-06-08 22:38:49 +0300

    a program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys
     total_args = 0
     for args in range(1, len(sys.argv)):
-        total_args += int(sys.argv[j])
+        total_args += int(sys.argv[args])
     print("{}".format(total_args))
