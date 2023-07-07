b2b1822e7f2b91a9087c9e0661bb7ef87aba22a2
yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-07-07 16:03:50 +0300

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Defines a name-printing function."""
+
+"""
+Defines a name-printing function.
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Print a name.
+    """
+    Print a name.
 
     Args:
         first_name (str): The first name to print.
         last_name (str): The last name to print.
+
     Raises:
         TypeError: If either of first_name or last_name are not strings.
     """
@@ -15,4 +20,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+
     print("My name is {} {}".format(first_name, last_name))
