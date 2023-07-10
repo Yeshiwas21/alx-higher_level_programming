cc4700bbe4a92ce1b106cd09f99592552a8cff10
yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-07-10 14:18:44 +0300

    a function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A function, lookup.
+"""
+
+
+def lookup(obj):
+    """
+    A function that returns a list of variables.
+    Args:
+        obj
+    """
+
+    return dir(obj)
