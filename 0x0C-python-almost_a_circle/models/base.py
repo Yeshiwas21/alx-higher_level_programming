298d6886d73fefcddbf408f0906f41de45ec2f51
yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-07-20 22:39:31 +0300

    Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -65,3 +65,65 @@ class Base:
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances of type caller class from a file"""
+        filename = cls.__name__ + ".json"
+
+        try:
+            with open(filename, encoding="utf-8") as myfile:
+                rd = myfile.read()
+                dicst = cls.from_json_string(rd)
+                inslist = []
+                for i in dicst:
+                    inslist.append(cls.create(**i))
+                return inslist
+        except IOError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ Serializes CSV and saves """
+        filename = cls.__name__ + ".csv"
+        csvlist = []
+        if list_objs:
+            for i in list_objs:
+                dic = i.to_dictionary()
+                if cls.__name__ == "Rectangle":
+                    csvlist.append([dic["id"], dic["width"],
+                                   dic["height"], dic["x"], dic["y"]])
+
+                elif cls.__name__ == "Square":
+                    csvlist.append([dic["id"], dic["size"],
+                                   dic["x"], dic["y"]])
+
+        with open(filename, "w", encoding="utf-8") as f:
+            w = csv.writer(f)
+            w.writerows(csvlist)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ Deserializes CSV and load """
+        filename = cls.__name__ + ".csv"
+
+        try:
+            with open(filename, encoding="utf-8") as f:
+                r = csv.reader(f)
+                if cls.__name__ == "Rectangle":
+                    att = ["id", "width", "height", "x", "y"]
+                elif cls.__name__ == "Square":
+                    att = ["id", "size", "x", "y"]
+                inslist = []
+                for row in r:
+                    ct, dic = 0, {}
+                    for i in row:
+                        dic[att[ct]] = int(i)
+                        ct += 1
+                    inslist.append(cls.create(**dic))
+                return inslist
+        except IOError:
+            return []
+
+    def draw(list_rectangles, list_squares):
+        """draws all rectangles and squares"""
