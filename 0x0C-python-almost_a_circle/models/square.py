2e64252689cb4ef6837bfbe3aed66567aedbd324
yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-07-20 22:18:47 +0300

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -71,9 +71,9 @@ class Square(Rectangle):
     def to_dictionary(self):
         """Returns the dictionary representation of a Square"""
         square_dict = {
-        'id': self.id,
-        'size': self.size,
-        'x': self.x,
-        'y': self.y
-        }
+                'id': self.id,
+                'size': self.size,
+                'x': self.x,
+                'y': self.y
+                }
         return square_dict
