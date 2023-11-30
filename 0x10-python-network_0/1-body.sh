6d2ba423cee4f6dc0af1d663f590010043cc759b
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2023-11-30 22:35:50 +0300

    Mandatory tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Get the response body for a given URL for 200 status code responses.
-curl -sLw "%{http_code}" "$1" -o /dev/null | {
-    read code
-    if [ "$code" -eq 200 ]; then
-        curl -sL "$1"
-    fi
-}
+response=$(curl -sL -w "%{http_code}" "$1")
+if [[ $response == *"200"* ]]; then
+    echo "$response" | sed -n '/^\s*$/,$p' | tail -n +2
+fi
