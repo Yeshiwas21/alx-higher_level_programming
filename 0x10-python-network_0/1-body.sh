267d73840c25fa33286106b6a2efb0feb297956a
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2023-11-30 21:50:49 +0300

    Mandatory tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request, and 
-# displays the body of the response for a 200 status code
-curl -sL -w "%{http_code}" "$1" -o /dev/null | {
-    read code
-    if [ "$code" -eq 200 ]; then
-        curl -s "$1"
-    fi
-}
+# This script takes in a URL, sends a GET request, and displays the body of the response for a 200 status code
+response=$(curl -sL -w "%{http_code}" "$1")
+if [[ $response == *"200"* ]]; then
+    echo "$response" | sed -n '/^\s*$/,$p' | tail -n +2
+fi
