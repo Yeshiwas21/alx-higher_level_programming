486974437c69dc60a8396a03ef50d1005d443691
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2023-12-04 14:33:41 +0300

    2-6

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""Fetches https://alx-intranet.hbtn.io/status using requests"""
+
 import requests
 
+url = "https://alx-intranet.hbtn.io/status"
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+response = requests.get(url)
+content = response.text
+
+print("Body response:")
+print("\t- type: {}".format(type(content)))
+print("\t- content: {}".format(content))
