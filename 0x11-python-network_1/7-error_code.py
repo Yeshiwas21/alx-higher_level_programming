43b4c18447090f246e796598fda5c7893cf19698
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2023-12-04 14:39:05 +0300

    7-10

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,10 +8,9 @@ import sys
 
 url = sys.argv[1]
 
-response = requests.get(url)
-content = response.text
-
-if response.status_code >= 400:
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
+    print(response.text)
+except requests.exceptions.HTTPError as err:
     print("Error code:", response.status_code)
-else:
-    print(content)
