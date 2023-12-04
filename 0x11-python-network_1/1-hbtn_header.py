ca32d92e3b7c9765b18573659c838d0ea118e647
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2023-12-04 14:26:45 +0300

    1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request, and displays the value of the X-Request-Id variable in the header"""
-
+"""Takes in a URL, sends a request, and displays the value of the X-Request-Id
 import urllib.request
 import sys
 
